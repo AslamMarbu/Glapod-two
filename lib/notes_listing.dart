578c080097103f22bb44b69a9282d4355f493099
aslam <aslammarbu404@gmail.com>
2026-05-21 12:44:53 +0530

    learning

--- lib/notes_listing.dart
+++ lib/notes_listing.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'widgets.dart/appbar_page.dart';
 import 'services/student_service.dart';
 
-class NotesListingPage  extends StatelessWidget {
-  const ({super.key});
+class NotesListingPage extends StatelessWidget {
+  const NotesListingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
